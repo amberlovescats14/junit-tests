431932e7af5f88eca053a7d72957040e75ead603
Amber Jones <amberlovescats14@gmail.com>
2019-12-16 15:56:48 -0600

    refactored exercise one

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -6,14 +6,14 @@ import java.util.stream.Stream;
 public class Student implements I_Student{
     private Long id;
     private String name;
-    public ArrayList<Integer> grades = new ArrayList<>();
+    private ArrayList<Integer> grades = new ArrayList<>();
 
     public Student(String name) {
         Random id = new Random();
         this.id = id.nextLong();
         this.name = name;
     }
-
+    //z: Change to primitive
     private int[] changeListToPrimitive(){
         int[] primitive = new int[grades.size()];
         for (int i = 0; i < grades.size(); i++) {
@@ -21,6 +21,8 @@ public class Student implements I_Student{
         }
         return primitive;
     }
+
+    //z: get length of
     public int getGradesLength(){
         return grades.size();
     }
@@ -42,6 +44,7 @@ public class Student implements I_Student{
 
     @Override
     public double getGradeAverage() {
+        if(grades.size() == 0 ) return (double)0;
         long total = grades.stream().reduce(0,(a,b)-> a+b);
         return total / grades.size();
     }
@@ -54,6 +57,10 @@ public class Student implements I_Student{
 
     @Override
     public int[] updateGrade(int oldGrade, int newGrade) {
+        if(grades.size() == 0 ) {
+            System.out.println("No grades entered");
+            return new int[0];
+        }
         try {
         if(!grades.contains(oldGrade)) throw new Exception();
 
@@ -68,6 +75,10 @@ public class Student implements I_Student{
 
     @Override
     public int[] deleteGrade(int grade) {
+        if(grades.size() == 0 ) {
+            System.out.println("No grades entered");
+            return new int[0];
+        }
         try {
             if(!grades.contains(grade)) throw new Exception();
 
